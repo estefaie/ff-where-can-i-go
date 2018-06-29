1b46f2fbca77e0b3bb4fc4ba2f4c602dc8b5e577
Hemen Estefaie <estefaie@gmail.com>
2018-06-29 11:08:03 +1000

    centre in mascot

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -2,22 +2,90 @@ import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeMarker: {},
+      selectedPlace: {},
+      locations: this.props.locations,
+      initialCenter: this.props.initialCenter,
+      showingInfoWindow: false
+    };
+  }
+
+  onMarkerClick = (props, marker) =>
+    this.setState({
+      activeMarker: marker,
+      selectedPlace: props,
+      showingInfoWindow: true
+    });
+
+  onInfoWindowClose = () =>
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false
+    });
+
+  onMapClicked = () => {
+    if (this.state.showingInfoWindow)
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false
+      });
+  };
   render() {
+    if (!this.props.loaded) return <div>Loading...</div>;
+
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map
+        className="map"
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        style={{ height: '100%', position: 'relative', width: '100%' }}
+        initialCenter = {this.state.initialCenter}
+        zoom={14}>
 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+        {/* <Marker
+          name="SOMA"
+          onClick={this.onMarkerClick}
+          position={{ lat: 37.778519, lng: -122.40564 }}
+        />
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>name</h1>
-            </div>
+        <Marker
+          name="Dolores park"
+          onClick={this.onMarkerClick}
+          position={{ lat: 37.759703, lng: -122.428093 }}
+        /> */}
+
+        <Marker
+          name="Current location"
+          onClick={this.onMarkerClick}
+          position={this.state.initialCenter}
+        />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          onClose={this.onInfoWindowClose}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+
+        <InfoWindow position={this.props.initialCenter} visible>
+          <small>
+            Click on any of the markers to display an additional info.
+          </small>
         </InfoWindow>
       </Map>
     );
   }
 }
+
+MapContainer.defaultProps = {
+  initialCenter: { lat: -33.925356, lng: 151.186735 },
+};
 
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyDpgoPu5yKXRzArjgIEDAyRprrxr7MzoDs')
